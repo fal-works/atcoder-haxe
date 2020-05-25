3ec5f504c9a14970a0847f4b2e4bc13fad11f69b
FAL <falworks.contact@gmail.com>
2020-05-26 03:46:04 +0900

    ABC081: A

---
+++ abc081/A/Main.hx
@@ -0,0 +1,53 @@
+class Main {
+	static function main() {
+		final cin = new CharIn();
+		var count = 0;
+		for (i in 0...3)
+			count += cin.digit();
+		Sys.println(count);
+	}
+}
+
+/**
+	wronganswer (naive) / CC0
+	https://github.com/fal-works/wronganswer
+**/
+
+abstract CharIn(haxe.io.Input) {
+	public extern inline function new()
+		this = Sys.stdin();
+
+	public inline function byte()
+		return this.readByte();
+
+	public inline function digit()
+		return byte() - "0".code;
+
+	public inline function str(delimiter:Int) {
+		final buffer = new haxe.io.BytesBuffer();
+		try {
+			var byte:Int;
+			while ((byte = this.readByte()) != delimiter) {
+				buffer.addByte(byte);
+			}
+		} catch (e:haxe.io.Eof) {}
+
+		return buffer.getBytes().toString();
+	}
+
+	public inline function int(delimiter:Int):Int
+		return Ut.atoi(str(delimiter));
+}
+
+enum abstract Delimiter(Int) to Int {
+	final LF = "\n".code;
+	final SP = " ".code;
+}
+
+class Ut {
+	@:noUsing public static macro function debug(message:haxe.macro.Expr):haxe.macro.Expr
+		return macro null;
+
+	@:pure public static inline function atoi(s:String):Int
+		return Std.parseInt(s);
+}
